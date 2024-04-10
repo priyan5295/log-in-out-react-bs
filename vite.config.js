0090723e0a0930e2379a5827179c38c47f50124d
Sathiyapriyan <sathiyapriyan@localhost.localdomain>
2024-04-10 18:39:27 +0530

    firsts commits

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/log-in-out-react-bs/",
+  assetsDir: 'assets',
   plugins: [react()],
 })
